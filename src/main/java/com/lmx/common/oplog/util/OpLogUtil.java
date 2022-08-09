54a72217126912228156dbb6281ec6d70010d2bf
limingxin <limingxin@basestonedata.com>
2022-08-09 11:26:08 +0800

    日志加入skyWalking trade-id

--- src/main/java/com/lmx/common/oplog/util/OpLogUtil.java
+++ src/main/java/com/lmx/common/oplog/util/OpLogUtil.java
@@ -1,8 +1,8 @@
 package com.lmx.common.oplog.util;
 
-import com.lmx.common.oplog.OpLog;
 import com.lmx.common.oplog.OperationLog;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.skywalking.apm.toolkit.trace.TraceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -16,19 +16,6 @@ public class OpLogUtil {
      */
     private static Logger log_es = LoggerFactory.getLogger("es_log");
 
-    public static void printLog(List l, OpLog opLogCollect) {
-        OperationLog operationLog = new OperationLog();
-        //偶数为改前值，奇数为改后值
-        for (int i = 0; i < l.size(); ++i) {
-            String oldVal = (String) l.get(i++);//0 2
-            String newVal = (String) l.get(i++);//1 4
-            operationLog.setModifyValue(newVal);
-            operationLog.setOriginalValue(oldVal);
-            //json结构输出，让filebeats完成后续导入kafka
-            log_es.info(operationLog.toString());
-        }
-    }
-
     public static void printLog(List l) {
         log.info("modify data={}", l);
         OperationLog operationLog = new OperationLog();
@@ -41,8 +28,8 @@ public class OpLogUtil {
             operationLog.setAppName(System.getProperty("spring.application.name"));
             operationLog.setModifyValue(newVal);
             operationLog.setOriginalValue(arr[1]);
-            //json结构输出，让filebeats完成后续导入kafka
             operationLog.setTraceId(MDC.get("X-B3-TraceId"));
+            operationLog.setSkyWalkingTraceId(TraceContext.traceId());
             operationLog.setTableName((String) l.get(l.size() - 2));
             operationLog.setTableId((String) l.get(l.size() - 1));
             operationLog.setField(arr[0]);
